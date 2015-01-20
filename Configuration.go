29191f2dea16e24967edabf0bf5fd2421327d4ad
Alexander Vakanin <vakanin.alexander@abv.bg>
2015-01-20 18:16:15 +0200

    Modified Configuration.go and MainFrame.go

--- Configuration.go
+++ Configuration.go
@@ -1,17 +1,17 @@
-package main
+package generala
 
 type Configuration struct {
     mFile   string
     higscores   map[string]*HighScoreItem
 }
 
-type HighScoreItem {
+type HighScoreItem struct{
     score int
     name string
 }
 
 func NewConfiguration(file string) *Configuration{
-
+    return new(Configuration)
 }
 
 //Reads Open Generala configurations file and parses it.
@@ -26,7 +26,7 @@ func (c *Configuration) Save(file string) {
 
 //Checks if a given score quallifies to the high score list
 func (c *Configuration) IsHighScore(score int) bool {
-
+    return true
 }
 
 //Adds a given entry to the highscore list.
